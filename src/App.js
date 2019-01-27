c476e86286b0470e91f4edf3a29e331c9ee659cb
isaaclem <lem.chiew.choon@gmail.com>
2019-01-27 23:23:02 +0800

    Setup prettier, eslint

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
-  state = {
-    count: 0
-  };
+    state = {
+        count: 0
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Hello world!!</h1>
-        <h2 className={this.state.count > 10 ? 'warning' : null}>
-          Count: {this.state.count}
-        </h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Hello world!!</h1>
+                <h2 className={this.state.count > 10 ? 'warning' : null}>
+                    Count: {this.state.count}
+                </h2>
+                <button
+                    onClick={() =>
+                        this.setState(state => ({ count: state.count + 1 }))
+                    }
+                >
+                    +
+                </button>
+                <button
+                    onClick={() =>
+                        this.setState(state => ({ count: state.count - 1 }))
+                    }
+                >
+                    -
+                </button>
+                {this.state.count > 10 ? (
+                    <React.Suspense fallback={null}>
+                        <Warning />
+                    </React.Suspense>
+                ) : null}
+            </div>
+        )
+    }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
